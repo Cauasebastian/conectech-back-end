a724d28a8027086ee51b5714ccf7f8767595cb89
felipeoliveira25 <felipeoferreira.25@gmail.com>
2024-03-26 18:00:44 -0300

    adicionando as rotas para a navegação na aplicação

---
+++ Front-end/conectech/src/globals/globalstyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    overflow-x: hidden;
+    height: 100vh;
+    
+
+  
+
+  
+} 
+
+li{
+    list-style: none;
+ }
+`
+
+export default GlobalStyle
---
+++ Front-end/conectech/src/pages/TelaLogin/telalogin.jsx
@@ -0,0 +1,11 @@
+
+
+const TelaLogin = () => {
+    return(
+        <>
+            <h3>Tela Login</h3>
+        </>
+    )
+}
+
+export default TelaLogin
