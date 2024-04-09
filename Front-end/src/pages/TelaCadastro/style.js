b151214a7ed816b9016f23dfc240667d185ace5a
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-04-09 00:59:43 -0300

    projeto backend  funcionando

--- Front-end/src/pages/TelaCadastro/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const DivCadastro = styled.div`
-    background-image: linear-gradient(#003363  ,#010727);
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    
-`
